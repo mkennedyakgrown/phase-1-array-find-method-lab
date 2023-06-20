16208adbb8665f298a9b3708a9481782cb70c30e
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2023-06-20 16:26:19 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+function isW(element) {
+    if (element.result === "W") {
+        return element.year;
+    }
+}
+
+function superbowlWin(record) {
+    const win = record.find(isW);
+    if (win === undefined) {
+        return win;
+    } else {
+    return win.year;
+    }
+}
